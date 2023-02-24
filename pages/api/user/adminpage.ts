55223ca7166557df08e1f02defe6714b3e0ce21a
Lakshmanshankar <lakshmanashankrc@gmail.com>
2023-02-24 06:02:40 +0530

    fixing role based auth

--- pages/api/user/adminpage.ts
+++ pages/api/user/adminpage.ts
@@ -9,10 +9,12 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
     else {
         verify(token!, process.env.JWT_SECRET!, async (err, decoded) => {
             if (err) return res.status(401).json({ message: "You are not logged in" });
-            const { user }: { user: userDetailsType } = decoded as any;
-            if (user.role === "admin") res.status(200).json({ message: "You are an admin" });
-            if (user.role === "teacher") res.status(200).json({ message: "You are logged with teacher account" });
-            else res.status(401).json({ message: "You neither admin nor teacher" });
+            else {
+                const { user }: { user: userDetailsType } = decoded as any;
+                if (user.role === "admin") res.status(200).json({ message: "You are an admin" });
+                else if (user.role === "teacher") res.status(200).json({ message: "You are logged with teacher account" });
+                else res.status(401).json({ message: "You neither admin nor teacher" });
+            }
         })
     }
 }
